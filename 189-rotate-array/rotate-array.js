e90cd5ffb8b8c766f2548a4113dff6822f52613f
BadcBoss <35306045+guatom999@users.noreply.github.com>
2024-08-21 03:58:41 +0700

    Time: 87 ms (32.79%) | Memory: 59.8 MB (32.72%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    let rotateArr = new Array(nums.length)
+    let keep = 0
+    for (let i = 0; i < nums.length; i++) {
+        newIndex = (i+k) % nums.length
+        // console.log("new index is" , newIndex)
+        rotateArr[newIndex] = nums[i]
+    }
+
+    for(let i = 0 ; i < nums.length;i++){
+        nums[i] = rotateArr[i]
+    }
+
+    console.log("rotateArr is" , rotateArr)
+
+};
