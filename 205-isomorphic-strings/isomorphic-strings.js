e854a69c978caad58d6b30b66bc265177f91d566
BadcBoss <35306045+guatom999@users.noreply.github.com>
2024-09-30 03:50:43 +0700

    Time: 69 ms (21.35%) | Memory: 50.5 MB (69.39%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+
+    var sMap = new Map()
+    var tMap = new Map()
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (sMap.has(s[i]) && sMap.get(s[i]) !== t[i]) {
+            return false
+        }
+
+        if (tMap.has(t[i]) && tMap.get(t[i]) !== s[i]) {
+            return false
+        }
+
+        sMap.set(s[i], t[i])
+        tMap.set(t[i], s[i])
+
+    }
+
+
+    return true
+
+};
