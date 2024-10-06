793071c8e5c1d9dd85dccc60f144fb2af768b8ed
BadcBoss <35306045+guatom999@users.noreply.github.com>
2024-10-07 03:13:38 +0700

    Time: 75 ms (44.25%) | Memory: 54.2 MB (27.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,12 +4,8 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    // const notDupS = Array.from(new Set(s)).sort().join("");
-    // const notDupT = Array.from(new Set(t)).sort().join("");
     const notDupS = Array.from(s).sort().join("");
     const notDupT = Array.from(t).sort().join("");
-
-    console.log("notDupS", notDupS, "notDupT", notDupT)
 
     if (notDupS != notDupT) {
         return false
