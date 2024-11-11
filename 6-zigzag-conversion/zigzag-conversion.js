0d9df7491009c87ccf085852e6414adc2aba16de
BadcBoss <35306045+guatom999@users.noreply.github.com>
2024-11-11 16:54:06 +0700

    Time: 10 ms (58.20%) | Memory: 55 MB (65.55%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+
+    newNumRows = numRows === 1 ? s.length : numRows
+    beforeResult = new Array(newNumRows).fill("");
+    plus = true
+    toAdd = 0;
+    plus = true
+    for (let i = 0; i < s.length; i++) {
+        beforeResult[toAdd] += s[i]
+        if(toAdd === 0){
+            plus = true
+        }else if(toAdd === numRows -1){
+            plus = false
+        }
+        plus ? toAdd++ : toAdd--
+    }
+
+    return beforeResult.join("")
+};
