a9f8d54e3e0b3c75b336334dfdae588d2f7a3d4a
BadcBoss <35306045+guatom999@users.noreply.github.com>
2025-06-20 04:27:51 +0700

    Time: 27 ms (80.20%) | Memory: 69.2 MB (21.55%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,28 +2,12 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function (strs) {
-    var result = []
-    var sortMap = new Map
-    var sortResult = new Array
-    const newStr = strs.map((e, i) => {
-        let subSort = Array.from(e).sort().join("")
-        return subSort
-    })
-
-    newStr.forEach((v, i) => {
-        if (!sortMap.has(v)) {
-            sortMap.set(v, [strs[i]])
-        } else {
-            sortMap.get(v).push(strs[i])
-
-        }
-    })
-
-    for (let [key, value] of sortMap) {
-        result.push(value)
+var groupAnagrams = function(strs) {
+    const justNewMap = new Map()
+    for(let word of strs){
+        sortedWord = word.split('').sort().join()
+        !justNewMap.has(sortedWord) && justNewMap.set(sortedWord,[])
+        justNewMap.get(sortedWord).push(word)
     }
-
-    return result
-
+    return Array.from(justNewMap.values())
 };
