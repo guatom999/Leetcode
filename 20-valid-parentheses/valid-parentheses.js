41381f06b67044a83d2c549cd2a966490ed48d0f
BadcBoss <35306045+guatom999@users.noreply.github.com>
2025-07-02 12:10:31 +0700

    Time: 501 ms (5.10%) | Memory: 65.7 MB (5.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,19 +3,21 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    map = { '(': ')', '{': '}', '[': ']' }
+    maps = { ')': '(', '}': '{', ']': '[' }
     stack = []
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+        console.log(stack)
+        if (maps[s[i]] === undefined) {
             stack.push(s[i])
         } else {
-            if (stack.length === 0) {
+            if (stack[stack.length - 1] !== maps[s[i]]) {
                 return false
-            } else if (map[stack.pop()] !== s[i]) {
-                return false
+            } else {
+                stack.pop()
             }
         }
     }
 
-    return stack.length === 0 
+    return stack.length === 0
+
 };
