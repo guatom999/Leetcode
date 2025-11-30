63cff5ac9ae82e2c2f041ccf41a9715816e8645b
BadcBoss <35306045+guatom999@users.noreply.github.com>
2025-11-30 13:25:03 +0700

    Time: 30 ms (9.33%) | Memory: 56.8 MB (21.38%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    if(x == 0 || x == 1) {
+        return x
+    }
+    for(let i = 0 ; i <= x;i++){
+        if((i * i) <= x){
+            continue
+        }
+        console.log("i" , i)
+        return i - 1
+    }
+};
