956a941ca95a451113f78d2c948c564732fc39de
BadcBoss <35306045+guatom999@users.noreply.github.com>
2026-09-10 02:00:42 +0700

    Time: 1 ms (72.49%) | Memory: 56.1 MB (87.30%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.js
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.js
@@ -4,15 +4,13 @@
  */
 var findClosestNumber = function (nums) {
     closet = 0
-
     for (let i = 1; i < nums.length; i++) {
-        console.log(closet)
         if (Math.abs(nums[i]) == Math.abs(nums[closet])) {
-            if (nums[i] > nums[closet]){
+            if (nums[i] > nums[closet]) {
                 closet = i
             }
         }
-        else if(Math.abs(nums[i]) < Math.abs(nums[closet])) {
+        else if (Math.abs(nums[i]) < Math.abs(nums[closet])) {
             closet = i
         }
     }
