d66028bb80234ed572f250add0b2194f8fc7abc4
BadcBoss <35306045+guatom999@users.noreply.github.com>
2026-09-16 02:49:59 +0700

    Time: 55 ms (10.02%) | Memory: 52.8 MB (94.43%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -4,17 +4,16 @@
  * @return {string}
  */
 var mergeAlternately = function (word1, word2) {
-    maxLength = Math.max(word1.length, word2.length)
-    var result = ""
+    const maxLength = Math.max(word1.length, word2.length)
+    let res = ""
     for (let i = 0; i < maxLength; i++) {
-        if(word1[i]){
-            result += word1[i]
+        if (word1[i]) {
+            res += word1[i]
         }
-        if(word2[i]){
-            result += word2[i]
+        if (word2[i]) {
+            res += word2[i]
         }
     }
 
-    return result
-
+    return res
 };
