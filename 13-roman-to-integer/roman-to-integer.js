a1ee2c2693602469fd2a56731d4e4133f9c5f5bf
BadcBoss <35306045+guatom999@users.noreply.github.com>
2026-09-17 14:55:49 +0700

    Time: 4 ms (73.37%) | Memory: 60.9 MB (62.10%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -2,20 +2,17 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    const v = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
-    romanLength = s.length
-    result = 0
-    i = 0
-    while(i < romanLength){
-        if(v[s[i]] < v[s[i+1]]){
-            result += (v[s[i+1]] - v[s[i]])
-            i += 2
-        }else{
+var romanToInt = function (s) {
+    const v = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    var result = 0
+    for (let i = 0; i < s.length; i++) {
+        if (v[s[i]] < v[s[i + 1]]) {
+            result += v[s[i + 1]] - v[s[i]]
+            i = i + 1
+        } else {
             result += v[s[i]]
-            i ++
         }
     }
-
     return result
+
 };
